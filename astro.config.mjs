78b4e2a34e51a25d864395825023dd5e21b452da
Sebastian Luque <sebastian.luque.pl@gmail.com>
2024-10-29 17:50:06 -0500

    add url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import { loadEnv } from 'vite'
 
 import react from '@astrojs/react'
 
+const { WEBSITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: WEBSITE_URL,
   integrations: [
     tailwind({
       applyBaseStyles: false,
